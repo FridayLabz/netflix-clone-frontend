ac34550a36feeda6b567bc241fe0d4da9421cd91
nermindaif <nermindaif34@gmail.com>
2018-07-20 14:03:00 +0200

    added profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,157 +1,67 @@
 import React, {Component} from 'react';
 import AvatarEditor from 'react-avatar-editor';
-import SplitterLayout from 'react-splitter-layout';
 import {Button,Image,Grid,Row,Col} from 'react-bootstrap';
 
 export default class YourComponent extends React.Component {
+
+  theMovie() {
+    //  "write the code that will lead you to the movie when you click its picture"
+  }
   render() {
     var Name="Your Name",Age="Your Age",Email="Your E-mail",Gender="Male Or Female"
     var Address="Your Address"
 
-const line= {
-    width: '1px',
-    backgroundColor: 'black',
-    position:'absolute',
-    top: '0',
-    bottom: '0',
-    left: '400px',
-    height: '1000px',
-};
 const imageStyle={
-  position:'absolute',
-  top: '0',
+  position:'relative',
+  top: '20px',
   bottom: '0',
-  left: '100px',
+  left: '120px',
+  borderRadius: '125px',
+  height:'125px',
+  width:'125px'
 }
 const infoStyle={
-  position:'absolute',
+  position:'relative',
   color: 'white',
-  top: '170px',
+  top: '50px',
   bottom: '0',
   left: '140px',
-  font: 'normal normal 900 20px/2 sans-serif'
+  font: 'normal normal 1000 20px/2 sans-serif'
 }
   return (
-    <container>
 
-    <div className="rightPane" style={{position:'absolute',width:'400px',height:'1000px',backgroundColor:'rgba(130,130,130)',}}>
-      <div ClassName="image" style={imageStyle}>
-
-
-        <AvatarEditor
-          image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-          width={125}
-          height={125}
-          borderRadius={125}
-          color={[130,130,130]} // RGBA
-          rotate={0}
-          link='https://github.com/nfl/react-helmet'
-        />
-
-      </div>
-      <div className="PersonalInfo" style={infoStyle}>
-            {Name}
-            <br></br>
-            {Gender}, {Age}
-            <br></br>
-            {Address}
-            <br></br>
-            {Email}
-      </div>
+    <div style={{position: "relative"}}>
+    <div className="upperpane" style={{backgroundColor:'rgb(215,0,0)',position:'relative',height:'62px',width:'1500px', font: 'normal normal 1000 40px/2 sans-serif',color:'white',}}>Netflix</div>
+    <div className="leftAndRightPanes" style={{position:'relative',background: 'linear-gradient(to right,  rgba(130,130,130) 0%,black 75%,black 75%,black 100%)',width:'1500px',height:'800px'}}>
+    <img style={imageStyle} src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={imageStyle}/>
+    <div className="info" style={infoStyle}>
+      {Name}
+      <br></br>
+      {Gender}, {Age}
+      <br></br>
+      {Address}
+      <br></br>
+      {Email}
     </div>
-
-    <div className="leftpane" style={{psition:'absolute',width:'1500px',height:'1000px',backgroundColor:'black',left:'400px'}}>
-      <h2 className="Interestslist" style={{color:'white',position:'absolute',top: '50px',bottom: '0',right: '500px'}}>Interests</h2>
-          <AvatarEditor style={{position:'absolute',top: '100px',bottom: '0',right: '700px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-            color={[0, 0, 0]} // RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '100px',bottom: '0',right: '500px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-          color={[0, 0, 0]}// RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '100px',bottom: '0',right: '300px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-          color={[0, 0, 0]} // RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '100px',bottom: '0',right: '100px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-            color={[0, 0, 0]}// RGBA
-            rotate={0}
-          />
-      <h2 className="Watchedlist" style={{color:'white',position:'absolute',top: '250px',bottom: '0',right: '500px'}}>Watched</h2>
-          <AvatarEditor style={{position:'absolute',top: '300px',bottom: '0',right: '700px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-          color={[0, 0, 0]}// RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '300px',bottom: '0',right: '500px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-            color={[0, 0, 0]} // RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '300px',bottom: '0',right: '300px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-            color={[0, 0, 0]} // RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '300px',bottom: '0',right: '100px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-            color={[0, 0, 0]} // RGBA
-            rotate={0}
-          />
-      <h2 className="toWatchList" style={{color:'white',position:'absolute',top: '450px',bottom: '0',right: '500px'}}>To Watch</h2>
-          <AvatarEditor style={{position:'absolute',top: '500px',bottom: '0',right: '700px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-          color={[0, 0, 0]} // RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '500px',bottom: '0',right: '500px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-            color={[0, 0, 0]} // RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '500px',bottom: '0',right: '300px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-            color={[0, 0, 0]}// RGBA
-            rotate={0}
-          />
-          <AvatarEditor style={{position:'absolute',top: '500px',bottom: '0',right: '100px',}}
-            image="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            width={100}
-            height={100}
-          color={[0, 0, 0]} // RGBA
-            rotate={0}
-          />
-       <h2 className="statistics" style={{color:'white',position:'absolute',top: '650px',bottom: '0',right: '500px'}}>Statistics</h2>
-
+  <div className="movies" style={{position:'relative',left:'750px',bottom:'290px',font:'normal normal 1000 25px/2 sans-serif'}}>
+    <h2 className="Interestslist" style={{color:'white',position:'relative',top:'0px',bottom: '0',left:'0',right: '500px',}}>Interests</h2>
+      <button style={{position:'relative',backgroundColor:'transparent',border:'none',right:'150px',bottom:'10px',height:'0px',width:'0px',cursor:'pointer'}}><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',border:'none',right:'30px',bottom:'10px',height:'0px',width:'0px',cursor:'pointer'}}><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',left:'10px',right: '500px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',left:'100px',bottom:'10px',height:'0px',width:'0px',border:'none',cursor:'pointer'}} ><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"style={{position:'relative',right: '0px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',left:'220px',bottom:'10px',height:'0px',width:'0px',border:'none',cursor:'pointer'}}><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',left:'0px',right: '100px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+    <h2 className="Watchedlist" style={{color:'white',position:'relative',top: '0px',bottom: '0',right: '10px',}}>Watched</h2>
+      <button style={{position:'relative',backgroundColor:'transparent',right:'150px',bottom:'10px',height:'0px',width:'0px',cursor:'pointer',border:'none'}} ><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',border:'none',right:'30px',bottom:'10px',height:'0px',width:'0px',cursor:'pointer'}}><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',left:'10px',right: '500px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',left:'100px',bottom:'10px',height:'0px',width:'0px',border:'none',cursor:'pointer'}} ><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"style={{position:'relative',right: '0px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',left:'220px',bottom:'10px',height:'0px',width:'0px',border:'none',cursor:'pointer'}}><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',left:'0px',right: '100px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+    <h2 className="toWatchList" style={{color:'white',position:'relative',top: '0px',bottom: '0',right: '10px'}}>To Watch</h2>
+      <button style={{position:'relative',backgroundColor:'transparent',right:'150px',bottom:'10px',height:'0px',width:'0px',cursor:'pointer',border:'none'}} ><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',border:'none',right:'30px',bottom:'10px',height:'0px',width:'0px',cursor:'pointer'}}><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',left:'10px',right: '500px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',left:'100px',bottom:'10px',height:'0px',width:'0px',border:'none',cursor:'pointer'}} ><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"style={{position:'relative',right: '0px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
+      <button style={{position:'relative',backgroundColor:'transparent',left:'220px',bottom:'10px',height:'0px',width:'0px',border:'none',cursor:'pointer'}}><img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" style={{position:'relative',left:'0px',right: '100px',height:'100px',width:'100px'}} onClick={this.theMovie}/></button>
     </div>
-  </container>
+  </div>
+</div>
     );
   }
 }
