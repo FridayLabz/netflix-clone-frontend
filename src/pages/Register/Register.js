f4610eb5109fcd03eadd7a54a1349c677e3861cb
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-08-10 17:16:58 +0200

    Implemented Register Page

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import * as EmailValidator from 'email-validator';
+import * as PasswordValidator from 'password-validator';
+import './Register.css';
+
+export default class Register extends Component {
+  constructor() {
+    super();
+    this.state = { email: '', password: '' };
+  }
+
+  onEmailUpdate = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  onPasswordUpdate = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+  validPassword = password => {
+    const schema = new PasswordValidator();
+
+    schema
+      .is()
+      .min(8)
+      .is()
+      .max(100)
+      .has()
+      .uppercase()
+      .has()
+      .lowercase()
+      .has()
+      .digits()
+      .has()
+      .symbols()
+      .has()
+      .not()
+      .spaces();
+
+    return schema.validate(password);
+  };
+
+  onSubmit = async () => {
+    const { email, password } = this.state;
+
+    if (!EmailValidator.validate(email)) {
+      alert('Invalid Email');
+      return;
+    }
+
+    if (!this.validPassword(password)) {
+      alert('Invalid Password');
+      return;
+    }
+
+    const res = await fetch('http://localhost:3001/accounts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
+
+    if (res.status !== 201) {
+      alert('Error creating account, Please try again.');
+      return;
+    }
+    alert('Account created successfully.');
+    const data = await res.json();
+
+    // TODO: make use of the data
+    console.log(data);
+
+    // TODO: redirect to Home Page
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div className="container-fluid register-container px-4">
+        <h1 className="display-4 font-weight-bold">Register</h1>
+        <div className="mt-4">
+          <input
+            type="text"
+            className="form-control my-3"
+            value={email}
+            placeholder="Email"
+            onChange={this.onEmailUpdate}
+          />
+          <input
+            type="password"
+            className="form-control my-3"
+            value={password}
+            placeholder="Password"
+            onChange={this.onPasswordUpdate}
+          />
+          <button
+            type="submit"
+            className="btn btn-danger btn-block"
+            onClick={this.onSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
